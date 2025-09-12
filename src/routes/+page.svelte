46b2db55d645b7710b568e04b795f6f7a81a1f3f
vsheo <vireshsheoratan@gmail.com>
2025-09-12 15:31:34 +0200

    netlify host fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,36 @@
 <script>
     let {data} = $props();
-
     const person = data.person;
+
+    // const body = document.querySelector('body')
+    // const theme = document.querySelector('.change-theme')
+    // const font = document.querySelector('.change-font')
+
+    // let oldTheme = 'palette-1'
+    // let oldFont = 'font-1'
+
+
+    // document.querySelectorAll('.change-theme input[name="palette"]').forEach(radio => {
+    //     radio.addEventListener('change', () => {
+    //         let newTheme = radio.id
+    //         body.classList.remove(oldTheme)
+    //         body.classList.add(newTheme)
+    //         oldTheme = newTheme
+    //     })
+    // })
+
+    // theme.removeAttribute('hidden')
+
+    // document.querySelectorAll('.change-font input[name="font"]').forEach(radio => {
+    //     radio.addEventListener('change', () => {
+    //         let newFont = radio.id
+    //         body.classList.remove(oldFont)
+    //         body.classList.add(newFont)
+    //         oldFont = newFont
+    //     })
+    // })
+
+    // font.removeAttribute('hidden')
 </script>
 
 <h1>FDND Visitekaartje</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,12 @@
-// DEFAULT
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	}
-// };
-
-// export default config;
-
-
-// NETLIFY
 import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			// will create a Netlify Edge Function using Deno-based
-			// rather than using standard Node-based functions
-			edge: true
-		})
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
